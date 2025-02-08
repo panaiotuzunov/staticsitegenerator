9a2c6eaf87553a3b8767b2536826bbec97a4ec75
Panaiot Uzunov <panaiotuzunov@gmail.com>
2025-02-08 18:18:26 +0200

    update splitnodes, add new functions

--- src/main.py
+++ src/main.py
@@ -5,5 +5,4 @@ def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
-
 main()
--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,16 +1,30 @@
+import re
 from textnode import TextType,TextNode
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
         else:                               #TODO Nesting of inline elements
-            split_nodes_text = node.text.split(delimiter, maxsplit=2)
-            new_nodes.extend(
-                [TextNode(split_nodes_text[0], TextType.TEXT),
-                TextNode(split_nodes_text[1], text_type),
-                TextNode(split_nodes_text[2], TextType.TEXT)]
-            )         
-    return new_nodes    
+            sections = node.text.split(delimiter)
+            if len(sections) % 2 == 0:
+                raise Exception("Invalid markdown syntax")
+            else:
+                for index in range(len(sections)):
+                    if sections[index] == "":
+                        continue
+                    if index % 2 == 0:
+                        new_nodes.append(TextNode(sections[index], TextType.TEXT))
+                    else:
+                        new_nodes.append(TextNode(sections[index], text_type))
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)      
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)  
