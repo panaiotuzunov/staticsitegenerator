a4d73d3b856978c5cb1d90a49275f30cb9d102e9
panaiotuzunov <panaiot.uzunov@gmail.com>
2025-02-18 17:28:12 +0200

    minor changes

--- src/copy_private_to_public.py
+++ src/copy_private_to_public.py
@@ -1,21 +1,25 @@
 from shutil import rmtree, copy
 from os import path, listdir, mkdir
 
+
 def delete_public():
     if path.exists("public/"):  # works only when run from project root                           
         rmtree("public/")       # deletes the folder public and all content
         mkdir("public/")        # creates a new public folder
         print("Succesfully deleted contents of public folder")
     else:
-        raise Exception("public folder doesn't exist")
+        mkdir("public/")        # creates a new public folder
+
 
 def copy_static_to_public(current_path="static/", dest_path="public/"):
     if path.exists(current_path):
         for item in listdir(current_path):
             source_file = path.join(current_path, item)
             dest_file = path.join(dest_path, item)
+            print(f"Copying {source_file} to {dest_file}")
             if path.isfile(source_file):
                 copy(source_file, dest_file)
                 continue
             mkdir(dest_file)
+            print(dest_file)
             copy_static_to_public(source_file, dest_file)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
 from textnode import TextNode, TextType
 from copy_private_to_public import delete_public, copy_static_to_public
-
+from block_markdown import markdown_to_blocks
 
 def main():
-    delete_public()
-    copy_static_to_public()    
+    # delete_public()
+    # copy_static_to_public()  
+    h1_header = extract_title(get_file_contents("index.md"))
+    print(h1_header)
+    
+    
+def extract_title(markdown):    
+    h1_header = ''.join(filter(lambda block: block.startswith("# "), markdown_to_blocks(markdown)))
+    if not h1_header:
+        raise Exception("h1 header not found")
+    return h1_header.split(maxsplit=1)[1]
+    
+
+def get_file_contents(path):
+    with open(path) as file:
+        return file.read()
 
 
 main()
